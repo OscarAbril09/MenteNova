7b469c873a5489e58b42f18e0201ddf8f6f0de6a
Dsgathebest <gomezabrildaniel1234@gmail.com>
2023-04-29 17:43:04 -0500

    feat: :sparkles: Componentes

--- sintomas/components/main.js
+++ sintomas/components/main.js
@@ -1,21 +1,12 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>User sintomas</title>
-    <link rel="stylesheet" href="../css/styles.css">
-    <!-- links bootstrap -->
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
-    <script src="main.js" type="module" defer></script>
-    <script src="/sintomas/components/main.js" type="module" defer></script>
+class MainSintomas extends HTMLElement {
+    constructor(){
+        super();
+        this.render();
+    }
 
-</head>
-<body>
-    <main-sintomas></main-sintomas>
-    <!-- <main class="d-flex">
+render(){
+    this.innerHTML=/*html*/`
+    <main class="d-flex">
         <aside class="col-12 col-md-4 col-lg-4 d-flex flex-column justify-content-center align-items-center">
             <div class="row d-flex justify-content-center align-items-center">
                 <div class="col-5">
@@ -99,6 +90,9 @@
                 </div>
             </div>
         </section>
-    </main> -->
-</body>
-</html>
+    </main>
+    `
+}}
+
+// Asociar
+customElements.define("main-sintomas",MainSintomas);
